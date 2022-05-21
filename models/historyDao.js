25aff18945de617c5785c54b0a7b0465950f4f42
HANSOL JEONG <losnah24@gmail.com>
2022-05-21 21:45:28 +0900

    FIX: 등록 차량 id로 요청 내역 상태를 업데이트 할 수 있도록 수정

--- models/historyDao.js
+++ models/historyDao.js
@@ -2,13 +2,24 @@ const { Prisma, PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
+// 등록 차량 id로 해당하는 progress_id 가져오기
+const getProgressId = async (carId) => {
+  const progressId = await prisma.$queryRaw`
+    SELECT progress_id
+    FROM registered_cars
+    WHERE id = ${carId};
+	`;
+
+  return progressId[0].progress_id;
+};
+
 // 해당하는 progress_id 에 요청값으로 절차명을 받아 업데이트 된 시간을 저장
-const updateProgress = async (id, progress) => {
+const updateProgress = async (progressId, progress) => {
   return await prisma.$queryRaw`
 		UPDATE progresses
     SET ${Prisma.raw(progress)} = now()
-    WHERE id = ${id}
+    WHERE id = ${progressId}
 	`;
 };
 
-module.exports = { updateProgress };
+module.exports = { getProgressId, updateProgress };
--- services/historyService.js
+++ services/historyService.js
@@ -1,8 +1,11 @@
 const historyDao = require("../models/historyDao");
 
 // 요청 내역 업데이트 및 승인 시간 생성
-const updateProgress = async (id, progress) => {
-  return await historyDao.updateProgress(id, progress);
+const updateProgress = async (carId, progress) => {
+  // 등록 차량 id로 요청 절차 id 가져오기
+  const progressId = await historyDao.getProgressId(carId);
+
+  return await historyDao.updateProgress(progressId, progress);
 };
 
 module.exports = {
