f8533c07eaeb050d2b11e570f966428515aba7ef
HANSOL JEONG <losnah24@gmail.com>
2022-05-27 15:23:46 +0900

    REFACTOR: 이미지 업로드 하나의 함수로 동작하도록 수정

--- models/imageDao.js
+++ models/imageDao.js
@@ -2,24 +2,14 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-// 차량 번호로 등록 차량 id 가져오기
-const getCarIdByCarNumber = async (carNumber) => {
-  const registeredCarId = await prisma.$queryRaw`
-		SELECT rc.id
-		FROM registered_cars rc
+// 이미지 업로드
+const uploadImages = async (carNumber, imageArray) => {
+  await prisma.$queryRaw`
+		UPDATE registered_cars rc
 		JOIN cars c ON rc.car_id = c.id
+		SET rc.image = ${imageArray}
 		WHERE c.car_number = ${carNumber}
 	`;
-  return registeredCarId[0].id;
 };
 
-// 이미지 업로드
-const uploadImages = async (registeredCarId, imageArray) => {
-  await prisma.$queryRaw`
-		UPDATE registered_cars
-		SET image = ${imageArray}
-		WHERE id = ${registeredCarId}
-	`;
-};
-
-module.exports = { uploadImages, getCarIdByCarNumber };
+module.exports = { uploadImages };
